2921937eb3615e0c4aed7ebea47e6b263c082dce
fernando <fernandogonzalezcruz2004@users.noreply.github.com>
2023-03-15 22:04:34 -0700

    finished this etch-a-sketch thing (for now)

--- etch.js
+++ etch.js
@@ -1,25 +1,45 @@
-const canvas = document.querySelector('#canvas')
-const square = document.createElement('div');
-
-
 
 function changeclr(event){
     console.log('clickedlol')
 }
-canvas.appendChild(square)
-for (let index = 0; index < 75*75; index++) {
-    const square = document.createElement('div')
-    dim = 'height:'+ String(960 / 75) +'px;'
-    dime = 'width:' + String(960 / 75) + 'px;'
-    console.log(dim)
-    square.classList.add('div')
-
+function set_easel(dimension){
+    if(dimension > 100){
+        dimension = 100
+    }
+    const canvas = document.querySelector('#canvas')
+    for (let index = 0; index < dimension * dimension; index++) {
+        const square = document.createElement('div')
+        dim = 'height:'+ String(960 / dimension) +'px;'
+        dime = 'width:' + String(960 / dimension) + 'px;'
+        console.log(dim)
+        square.classList.add('div')
     
-    square.style.cssText = dim+ dime+'background-color: white;border-radius: 5px;'
-    square.addEventListener('mouseover',function(e)  {
-       e.target.style.background = 'black';
-    })
-    
-    canvas.appendChild(square);
-    
+        
+        square.style.cssText = dim+ dime+'background-color: white;border-radius: 5px;'
+        square.addEventListener('mouseover',function(e)  {
+           const randomColor = Math.floor(Math.random()*16777215).toString(16);
+           e.target.style.backgroundColor = '#'+ randomColor;
+        })
+        
+        canvas.appendChild(square);
+        
+    }
 }
+
+
+
+set_easel(75)
+
+const reset = document.querySelector('#btn')
+btn.addEventListener('click',() => {
+    var num = parseInt(prompt('enter a dimension'))
+    if(isNaN(num)){
+        num = 50
+    }
+    console.log(num)
+    const canvas = document.querySelector('#canvas')
+    while(canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+    set_easel(num)
+})
